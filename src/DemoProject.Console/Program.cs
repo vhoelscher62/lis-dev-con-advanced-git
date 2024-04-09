2ef29d5f4db45dca1c83bcf639c5a971b2d18633
vhoelscher <vhoelscher@web.de>
2024-04-09 14:35:46 +0200

    Second Chnage

--- src/DemoProject.Console/Program.cs
+++ src/DemoProject.Console/Program.cs
@@ -17,13 +17,13 @@ public class Program
 
     if (Match.Success)
     {
-      int Year = Convert.ToInt32(Match.Groups[0].Value.Split('-')[0]); 
+      int Year = Convert.ToInt32(Match.Groups[0].Value.Split('-')[0]);
       int Month = Convert.ToInt32(Match.Groups[0].Value.Split('-')[1]);
       int Day = Convert.ToInt32(Match.Groups[0].Value.Split('-')[2]);
 
       Console.WriteLine($"The {Input} was a {CalculateDayOfWeek(Year, Month, Day)}");
     }
-    else 
+    else
       Console.WriteLine("WRONG FORMAT");
   }
 
@@ -33,7 +33,7 @@ public class Program
       year--;
 
     int weekDay = Convert.ToInt32((
-      (day + Math.Floor(2.6 * ((month + 9) % 12 + 1 - 1) - 0.2) + year % 100 
+      (day + Math.Floor(2.6 * ((month + 9) % 12 + 1 - 1) - 0.2) + year % 100
       + Math.Floor(((double)year % 100 / 4)) + Math.Floor((double)year / 400) - 2
       * Math.Floor((double)year / 100) - 1) % 7 + 7) % 7 + 1);
 
